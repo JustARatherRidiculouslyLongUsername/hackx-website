bdf00de1cc5dfeab505f8a3821d6e57664032f5b
Viswas Haridas <viswas.b10@gmail.com>
2022-07-03 18:10:42 +0530

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Hack-X</title>
+        <meta name="description" content="The cybersecurity club of MIT-WPU" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Hack-X
+        </h1>
+
+        <p className={styles.description}>
+          The cybersecurity club of MIT-WPU
+        </p>
+      </main>
+    </div>
+  )
+}
