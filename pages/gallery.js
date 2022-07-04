054d0c847fb1362231f78b456220ba0dcb7caebb
Bisleri06 <flakjacket06@protonmail.com>
2022-07-04 23:47:10 +0530

    Added Gallery

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,17 +1,38 @@
+import { Box,ImageList,ImageListItem } from '@mui/material'
 import Head from 'next/head'
+
+const itemData=[
+  "/img1.jpg",
+  "/img2.jpg",
+  "/img3.jpg",
+  "/img4.jpg",
+  "/img5.jpg",
+  "/img6.jpg",
+  "/img7.jpg",
+  "/img8.jpg",
+  "/img9.jpg"
+]
 
 export default function Gallery() {
   return (
-    <div className="container">
+    <>
       <Head>
         <title>Gallery</title>
       </Head>
 
-      <main className="main">
-        <h1 className="title">
-          Gallery
-        </h1>
-      </main>
-    </div>
+      {/* replace this with header */}
+      <Box sx={{position:"relative",color:"white",left:0,top:0,width:"100%",height:"4rem"}}>HEADER</Box>
+
+      <ImageList cols={3} rowHeight={400} sx={{overflow:"hidden"}}>
+        {itemData.map((item) => (
+          <ImageListItem>
+            <img
+              src={item}
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </>
   )
 }
